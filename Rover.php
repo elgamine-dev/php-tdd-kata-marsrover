8995097f3d8eb90fbec5a810f4cfe99e73223423
Amine El Ghayate <mhwebdev@gmail.com>
2017-01-26 14:45:17 +0100

    refactoring

--- Rover.php
+++ Rover.php
@@ -5,6 +5,12 @@ class Rover {
 	private $y;
 	private $dir;
 
+	private $directions = [
+		'cardinals'=> ['n','e','s','w'],
+		'x'=>[0, -1, 0, 1],
+		'y'=>[-1, 0, 1, 0]
+	];
+
 	public function __construct($x=0, $y=0, $dir='w'){
 		$this->x = $x;
 		$this->y = $y;
@@ -12,64 +18,36 @@ class Rover {
 	}
 
 	public function left(){
-		if($this->dir === 'n') {
-			return $this->dir = "w";
-		}
-		if($this->dir === 'w') {
-			return $this->dir = "s";
-		}
-		if($this->dir === 's') {
-			return $this->dir = "e";
-		}
-		if($this->dir === 'e') {
-			return $this->dir = "n";
-		}
-
+		$this->turn(true);
 	}
 
 	public function right(){
-		if($this->dir === 'n') {
-			return $this->dir = "e";
-		}
-		if($this->dir === 'e') {
-			return $this->dir = "s";
-		}
-		if($this->dir === 's') {
-			return $this->dir = "w";
-		}
-		if($this->dir === 'w') {
-			return $this->dir = "n";
+		$this->turn(false);
+	}
+
+	private function turn($left = true){
+		$shift = ($left) ? - 1 : 1;
+		$bound = ($left) ? count($this->directions['cardinals']) - 1 : 0;
+		$key = array_search($this->dir, $this->directions['cardinals']);
+		if(isset($this->directions['cardinals'][$key + $shift])) {
+			$this->dir = $this->directions['cardinals'][$key + $shift];
+		} else {
+			$this->dir = $this->directions['cardinals'][$bound];
 		}
 	}
 
 	public function forward(){
-		if($this->dir === 'w') {
-			$this->x = $this->x + 1;
-		}
-		if($this->dir === 'e') {
-			$this->x = $this->x - 1;
-		}
-		if($this->dir === 'n') {
-			$this->y = $this->y - 1;
-		}
-		if($this->dir === 's') {
-			$this->y = $this->y + 1;
-		}
+		$this->move(1);
 	}
 
 	public function backward(){
-		if($this->dir === 'w') {
-			$this->x = $this->x - 1;
-		}
-		if($this->dir === 'e') {
-			$this->x = $this->x + 1;
-		}
-		if($this->dir === 'n') {
-			$this->y = $this->y + 1;
-		}
-		if($this->dir === 's') {
-			$this->y = $this->y - 1;
-		}
+		$this->move(-1);
+	}
+
+	private function move($dir = 1){
+		$key = array_search($this->dir, $this->directions['cardinals']);
+		$this->x = $this->x + $this->directions['x'][$key] * $dir;
+		$this->y = $this->y + $this->directions['y'][$key] * $dir;
 	}
 
 
